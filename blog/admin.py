b76f671bcf829a39a2aba7e005064b6547a07c4f
Davide Fiacco <fcc.davide@gmail.com>
2023-02-09 17:55:07 +0100

    Added My-classes for arakne exp

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, MyTofI, MyPriority, MySOinP
 
 admin.site.register(Post)
+
+
+class MyTofIAdmin(admin.ModelAdmin):
+    list_display = ("content",)
+
+class MyPriorityAdmin(admin.ModelAdmin):
+    list_display = ("external_id", )
+
+class MySOinPAdmin(admin.ModelAdmin):
+    list_display = ("external_id", "priority",)
+    filter_horizontal = ( "intervention_type",)
+
+
+admin.site.register(MyTofI, MyTofIAdmin)
+admin.site.register(MyPriority, MyPriorityAdmin)
+admin.site.register(MySOinP, MySOinPAdmin)
--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,39 @@ class Post(models.Model):
 
      def __str__(self):
          return self.title
+     
+
+class MyTofI (models.Model):
+    external_id = models.CharField(max_length=500, null=True, blank=True)
+
+    content = models.TextField(null=False, blank=False, unique=True)
+
+    class Meta:
+        verbose_name_plural = ("MY Types of Intervention")
+
+    def __str__(self):
+        return self.content
+
+
+class MyPriority (models.Model):
+    external_id = models.CharField(max_length=500, null=True, blank=True)
+
+    intervention_type = models.ManyToManyField(
+        MyTofI, verbose_name="Type of Intervention", related_name="priorities", blank=True
+    )
+    class Meta:
+        verbose_name_plural = ("MY Priority")
+
+
+class MySOinP (models.Model):
+    external_id = models.CharField(max_length=500, null=True, blank=True)
+
+    priority = models.ForeignKey( 
+        MyPriority, related_name="specific_objective_in_priority", on_delete=models.SET_NULL, null=True
+    )
+    
+    intervention_type = models.ManyToManyField(
+        MyTofI, verbose_name="Type of Intervention", related_name="specific_objective_in_priority", blank=True
+    )
+    class Meta:
+        verbose_name_plural = ("MY SOinP")
